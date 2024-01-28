71ac61ed822b0ab73858b8ffe7f46e667f5d9a9e
becca-93 <155997078+becca-93@users.noreply.github.com>
2024-01-27 22:43:44 -0500

    Added html code and JavaScript code into each to repo

---
+++ script.js
@@ -0,0 +1,9 @@
+let firstName = "Rebecca";
+let lastName = "Nunez";
+let age = 30;
+
+alert("My name is " + firstName + " " + lastName);  
+
+alert("I am " + age + " years old");
+
+console.log("I am really excited with this class!");
